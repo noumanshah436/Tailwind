e6bb63e0348e6470ba33a541f70ef0531eced072
noumanshah436 <noumanshah436@gmail.com>
2023-10-05 00:55:16 +0500

    Review tailwind

---
+++ full_course/Basics/tailwind.config.js
@@ -0,0 +1,14 @@
+// this config file is just to use tailwind intellicence
+module.exports = {
+  mode: "jit",
+  darkMode: false,
+  content: ["*"],
+  theme: {
+
+    extend: {
+
+    },
+  },
+  plugins: [],
+};
+
